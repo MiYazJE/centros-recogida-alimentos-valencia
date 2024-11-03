82fd01393097f639752037036f5984f28c9cc047
ruben <ruben.saizserrano@gmail.com>
2024-11-03 18:10:36 +0100

    fix(): add new tag

--- src/enums/index.js
+++ src/enums/index.js
@@ -1,15 +1,16 @@
 export const TAGS = [
-  { value: "ropa", label: "Ropa" },
-  { value: "comida", label: "Comida" },
-  { value: "comida-animales", label: "Comida de animales" },
-  { value: "limpieza", label: "Productos de limpieza" },
-  { value: "ropa-seguridad", label: "Ropa de seguridad" },
-  { value: "medicamentos", label: "Medicamentos" },
-  { value: "comida-ninos", label: "Comida niños" },
-  { value: "ropa-ninos", label: "Ropa niños" },
-  { value: "higiene", label: "Higiene personal" },
-  { value: "herramientas", label: "Herramientas" },
-]
+  { value: 'ropa', label: 'Ropa' },
+  { value: 'comida', label: 'Comida' },
+  { value: 'comida-animales', label: 'Comida de animales' },
+  { value: 'animales-otros', label: 'Animales otros' },
+  { value: 'limpieza', label: 'Productos de limpieza' },
+  { value: 'ropa-seguridad', label: 'Ropa de seguridad' },
+  { value: 'medicamentos', label: 'Medicamentos' },
+  { value: 'comida-ninos', label: 'Comida niños' },
+  { value: 'ropa-ninos', label: 'Ropa niños' },
+  { value: 'higiene', label: 'Higiene personal' },
+  { value: 'herramientas', label: 'Herramientas' },
+];
 
 export const INITIAL_CORDS = [39.43333, -0.41667];
 
